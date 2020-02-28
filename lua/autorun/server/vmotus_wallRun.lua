9ac6b61e2d00857bcf9871a9377541e3178e197a
Nicolas Wasserkampf <nicolaswasserkampf@gmail.com>
2020-02-28 22:28:44 +0100

    tweaked wallrun values and added horizontal wallrunning

--- lua/autorun/server/vmotus_wallRun.lua
+++ lua/autorun/server/vmotus_wallRun.lua
@@ -9,6 +9,24 @@ local stepSounds = {
     "player/footsteps/concrete4.wav"
 }
 
+function wallRun(number, player, frontal)
+    local v = player:GetVelocity()
+    if v.z > -450 then
+        player:ViewPunch(Angle(-15, 0, 0))
+        player:EmitSound(stepSounds[math.random(#stepSounds)])
+        player:SetVelocity(Vector(0 - (frontal and v.x * 0.75 or 0), 0 - (frontal and v.y * 0.75 or 0), 300 - v.z))
+        steps = steps - 1
+
+        timer.Create(
+            number .. "wrcooldown",
+            0.25,
+            1,
+            function()
+            end
+        )
+    end
+end
+
 hook.Add(
     "Think",
     "vmotus_wrT",
@@ -24,25 +42,25 @@ hook.Add(
             local forwarder = Vector(5, 0, 0)
             forwarder:Rotate(Angle(0, cPlayer:EyeAngles().yaw, 0))
 
-            local collision = util.QuickTrace(cPlayer:GetPos() + Vector(0, 0, 60), forwarder * 8, cPlayer)
-            if collision.Hit then
-                if cPlayer:KeyDown(IN_FORWARD) and cPlayer:KeyDown(IN_SPEED) then
-                    if steps > 0 then
-                        if not timer.Exists(i .. "wrcooldown") then
-                            local z = cPlayer:GetVelocity().z
-                            cPlayer:EmitSound(stepSounds[math.random(#stepSounds)])
-                            cPlayer:SetVelocity(Vector(0, 0, 300-(z > 100 and z or 0)))
-                            steps = steps - 1
+            local collisionFront = util.QuickTrace(cPlayer:GetPos() + Vector(0, 0, 60), forwarder * 4, cPlayer)
+            local collisionRight =
+                util.QuickTrace(cPlayer:GetPos() + Vector(0, 0, 28), cPlayer:EyeAngles():Right() * 19, cPlayer)
+            local collisionLeft =
+                util.QuickTrace(cPlayer:GetPos() + Vector(0, 0, 28), -cPlayer:EyeAngles():Right() * 19, cPlayer)
+            local collisionUp = util.QuickTrace(cPlayer:GetPos(), Vector(0, 0, 120), cPlayer)
 
-                            timer.Create(
-                                i .. "wrcooldown",
-                                0.5,
-                                1,
-                                function()
-                                end
-                            )
-                        end
-                    end
+            if
+                not collisionUp.Hit and cPlayer:KeyDown(IN_FORWARD) and cPlayer:KeyDown(IN_SPEED) and steps > 0 and
+                    not timer.Exists(i .. "wrcooldown")
+             then
+                if collisionFront.Hit then
+                    wallRun(i, cPlayer, true)
+                end
+                if collisionLeft.Hit and cPlayer:KeyDown(IN_MOVELEFT) then
+                    wallRun(i, cPlayer, false)
+                end
+                if collisionRight.Hit and cPlayer:KeyDown(IN_MOVERIGHT) then
+                    wallRun(i, cPlayer, false)
                 end
             end
         end
