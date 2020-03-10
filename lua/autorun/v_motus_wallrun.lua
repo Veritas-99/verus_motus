e6d2979a7445f49df00d3f63a23d275f7f07f8c6
Nicolas Wasserkampf <nicolaswasserkampf@gmail.com>
2020-03-10 18:44:05 +0100

    horizontal wallrunning/jumping

--- lua/autorun/v_motus_wallrun.lua
+++ lua/autorun/v_motus_wallrun.lua
@@ -38,23 +38,40 @@ if SERVER then
 	hook.Add("PlayerPostThink", "v_motus_ppt_wallrun", function(ply)
 		//Enter parkour mode
 		if ply.v_motus then
-			local epos = ply:GetPos()
+			local ppos = ply:GetPos()
 			local eang = ply:EyeAngles()
 			local frwdr = Vector(5, 0, 0)
 			frwdr:Rotate(Angle(0, eang.yaw, 0))
 			local plyv = ply:GetVelocity()
-			local frontTrace = util.QuickTrace(epos + Vector(0, 0, 20), frwdr * 3.5, ply)
-			local ceilingTrace = util.QuickTrace(epos, Vector(0, 0, 100), ply)
+			local ceilingTrace = util.QuickTrace(ppos, Vector(0, 0, 100), ply)
+			local frontTrace = util.QuickTrace(ppos + Vector(0, 0, 20), frwdr * 3.5, ply)
+			local leftTrace = util.QuickTrace(ppos + Vector(0, 0, 28), -eang:Right() * 23, ply)
+			local rightTrace = util.QuickTrace(ppos + Vector(0, 0, 28), eang:Right() * 23, ply)
 
-			//Vertical segment
-			if !ceilingTrace.Hit && frontTrace.Hit then
+			if !ceilingTrace.Hit then
 				local wallRunCooldown = ply:AccountID() && ply:AccountID() .. "wrcd" || "wrcd"
 				local wallJumpCooldown = ply:AccountID() && ply:AccountID() .. "wrjp" || "wrjp"
 
-				if ply:KeyDown(IN_FORWARD) && !ply:KeyDown(IN_JUMP) && !timer.Exists(wallRunCooldown) && ply.v_motus_steps > 0 then
-					verticalWallRun(ply, plyv, wallRunCooldown)
-				elseif ply:KeyDown(IN_JUMP) && ply.v_motus_steps < v_motus_steps(ply) && !timer.Exists(wallJumpCooldown) then
-					halfRotationWallJump(ply, wallJumpCooldown)
+				//Vertical segment
+				if frontTrace.Hit then
+					if ply:KeyDown(IN_FORWARD) && !ply:KeyDown(IN_JUMP) && !timer.Exists(wallRunCooldown) && ply.v_motus_steps > 0 then
+						verticalWallRun(ply, plyv, wallRunCooldown)
+					elseif ply:KeyDown(IN_JUMP) && ply.v_motus_steps < v_motus_steps(ply) && !timer.Exists(wallJumpCooldown) then
+						halfRotationWallJump(ply, wallJumpCooldown)
+					end
+					//Horizontal segment
+				elseif leftTrace.Hit then
+					if ply:KeyDown(IN_FORWARD) && ply:KeyDown(IN_MOVELEFT) && !ply:KeyDown(IN_JUMP) && !timer.Exists(wallRunCooldown) && ply.v_motus_steps > 0 then
+						horizontalWallRun(ply, plyv, wallRunCooldown)
+					elseif ply:KeyDown(IN_JUMP) && ply.v_motus_steps < v_motus_steps(ply) && !timer.Exists(wallJumpCooldown) then
+						horizontalWallJump(ply, true, wallJumpCooldown)
+					end
+				elseif rightTrace.Hit then
+					if ply:KeyDown(IN_FORWARD) && ply:KeyDown(IN_MOVERIGHT) && !ply:KeyDown(IN_JUMP) && !timer.Exists(wallRunCooldown) && ply.v_motus_steps > 0 then
+						horizontalWallRun(ply, plyv, wallRunCooldown)
+					elseif ply:KeyDown(IN_JUMP) && ply.v_motus_steps < v_motus_steps(ply) && !timer.Exists(wallJumpCooldown) then
+						horizontalWallJump(ply, false, wallJumpCooldown)
+					end
 				end
 			end
 		end
@@ -72,11 +89,23 @@ if SERVER then
 		createWallRunCoolDown(coolDown)
 	end
 
+	//Horizontal Wall Run
+	function horizontalWallRun(ply, vel, coolDown)
+		ply.v_motus_steps = ply.v_motus_steps - 1
+		//
+		ply:ViewPunch(Angle(-15, 0, 0))
+		ply:EmitSound(stepsounds[math.random(#stepsounds)])
+		//
+		ply:SetVelocity(Vector(0, 0, (v_motus_step_force(ply) * 0.9091) - (vel.z >= 0 && vel.z || 0)))
+		//
+		createWallRunCoolDown(coolDown)
+	end
+
 	function createWallRunCoolDown(coolDown)
 		timer.Create(coolDown, 0.3, 1, function() end)
 	end
 
-	//Vertial Wall Jump
+	//Vertical Wall Jump
 	function halfRotationWallJump(ply, coolDown)
 		local preVel = -ply:EyeAngles():Forward()
 		preVel.z = 0
@@ -102,6 +131,32 @@ if SERVER then
 		net.Start("v_motus_turn")
 		net.WriteTable(v_motus_turnData)
 		net.Send(ply)
+		//
+		createWallJumpCoolDown(coolDown, ply)
+	end
+
+	//Horizontal Wall Jump
+	function horizontalWallJump(ply, left, coolDown)
+		local cVel = ply:GetVelocity()
+		preVel = cVel:GetNormalized()
+		preVel.z = 0
+
+		if left then
+			preVel:Rotate(Angle(0, -90, 0))
+		else
+			preVel:Rotate(Angle(0, 90, 0))
+		end
+
+		preVel.x = preVel.x - cVel:GetNormalized().x * 1.25
+		preVel.y = preVel.y - cVel:GetNormalized().y * 1.25
+		//
+		local jumpForce = v_motus_step_force(ply) * 0.9091
+		local vel = (preVel * (jumpForce * 0.6)) + Vector(0, 0, jumpForce - cVel.z)
+		//
+		ply:ViewPunch(Angle(-10, 0, 0))
+		ply:EmitSound(stepsounds[math.random(#stepsounds)])
+		//
+		ply:SetVelocity(vel)
 		//
 		createWallJumpCoolDown(coolDown, ply)
 	end
